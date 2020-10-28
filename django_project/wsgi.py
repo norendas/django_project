febc5265d143a39d65dfada870f033342e0bd9cc
norendas <norendas@gmail.com>
2020-10-28 08:40:18 +0530

    modified wsgi

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -8,8 +8,16 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+import site
 from django.core.wsgi import get_wsgi_application
+
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('/home/noren/Desktop/DjangoFolder/django_project/venv/lib/python3.7/site-packages')
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/home/noren/Desktop/DjangoFolder/django_project/django_project')
+sys.path.append('/home/noren/Desktop/DjangoFolder/django_project/django_project/')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
